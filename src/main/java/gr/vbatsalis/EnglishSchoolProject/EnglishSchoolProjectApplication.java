cb3bf302ca1cb0a1a509f8538db96d02862a662d
Evangelos Batsalis <vbatsalis@gmail.com>
2023-07-20 01:27:17 +0300

    First init commit

---
+++ src/main/java/gr/vbatsalis/EnglishSchoolProject/EnglishSchoolProjectApplication.java
@@ -0,0 +1,13 @@
+package gr.vbatsalis.EnglishSchoolProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishSchoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishSchoolProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/vbatsalis/EnglishSchoolProject/EnglishSchoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.vbatsalis.EnglishSchoolProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishSchoolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
