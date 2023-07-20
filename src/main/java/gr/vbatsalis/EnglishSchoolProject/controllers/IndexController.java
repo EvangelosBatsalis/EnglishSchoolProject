2065eee7424e7c956446d46073e72060d5816c46
Evangelos Batsalis <vbatsalis@gmail.com>
2023-07-21 00:27:03 +0300

    add two more html templates and corrected a bug to the controller

--- src/main/java/gr/vbatsalis/EnglishSchoolProject/controllers/IndexController.java
+++ src/main/java/gr/vbatsalis/EnglishSchoolProject/controllers/IndexController.java
@@ -1,13 +1,27 @@
 package gr.vbatsalis.EnglishSchoolProject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     public String IndexController() {
         return "index";
+    }
+
+    @RequestMapping("/e")
+    @GetMapping
+    public String emptyHandler(){
+        return "empty";
+    }
+
+    @RequestMapping("/test")
+    @GetMapping
+    public String testHandler(){
+        return "test";
     }
 
 }
