83fbca16d7e7fc0f46e76995efa16afba995a2da
Evangelos Batsalis <v.batsalis@gmail.com>
2023-07-21 21:53:02 +0300

    add html template

--- src/main/java/gr/vbatsalis/EnglishSchoolProject/controllers/IndexController.java
+++ src/main/java/gr/vbatsalis/EnglishSchoolProject/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package gr.vbatsalis.EnglishSchoolProject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,4 +25,10 @@ public class IndexController {
         return "test";
     }
 
+    @RequestMapping("/tmp")
+    @GetMapping
+    public String tmpHandler(Model model){
+        model.addAttribute("kati","Geia soy malaka!!");
+        return "indextmp";
+    }
 }
